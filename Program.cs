d246e2ab70bde892a054c1aeb1802d23173f8e35
Ro <adm.031@ya.ru>
2023-03-10 01:30:15 +0300

    Решение 4 задачи"

--- Program.cs
+++ Program.cs
@@ -4,14 +4,16 @@ Console.WriteLine("Введите число: ");
 int N = int.Parse(Console.ReadLine()!);
 int a = 1;
 
-   while (a<=N);
-    if (a%2==0)
+   while (a<=N)
+    {
+        if (a%2==0)
      Console.Write($"{a} ");
      a++;
+     }
 
  
-    
-    
+
+
   
    
 
