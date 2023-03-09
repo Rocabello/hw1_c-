cb0ad017bd4bfa83f5e4f5ea02a58e0206a67af2
Ro <adm.031@ya.ru>
2023-03-10 01:05:33 +0300

    Решение 4 задачи, неполное

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int a = 1;
+
+   while (a<=N);
+    if (a%2==0)
+     Console.Write($"{a} ");
+     a++;
+
+ 
+    
+    
+  
+   
+
+
+
