c74cb90458afc88e6d4224d34fdceeb2998370e6
Ro <adm.031@ya.ru>
2023-03-09 23:31:55 +0300

    Решение задачи 1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("First number: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.Write("Second number: ");
+int number2 = int.Parse(Console.ReadLine()!);
+if (number1 > number2) Console.WriteLine("First max");
+    else 
+    Console.WriteLine("Second max");
+
